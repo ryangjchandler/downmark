00bb7c1441b6aa93a7d46ab4c084fccc15d69e49
Ryan Chandler <ryangjchandler@gmail.com>
2022-04-22 12:36:27 +0100

    feature: custom render callbacks

--- src/Blocks/Block.php
+++ src/Blocks/Block.php
@@ -2,7 +2,16 @@
 
 namespace Downmark\Blocks;
 
+use Closure;
+
 abstract class Block
 {
     abstract public function toHtml(): string;
+
+    protected ?Closure $renderer = null;
+
+    public function renderUsing(Closure $callback): void
+    {
+        $this->renderer = $callback;
+    }
 }
--- src/Blocks/CodeBlock.php
+++ src/Blocks/CodeBlock.php
@@ -10,6 +10,10 @@ class CodeBlock extends Block
 
     public function toHtml(): string
     {
+        if ($this->renderer !== null) {
+            return call_user_func($this->renderer, $this);
+        }
+
         return sprintf("<pre><code %s>\n%s\n</code></pre>", $this->language ? sprintf('class="language-%s"', $this->language) : null, htmlentities($this->code));
     }
 }
